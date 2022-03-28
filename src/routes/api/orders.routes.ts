68d8b42eff91958054cd3f0c3d97bd04ceb9dd52
Ahmed Bahgat <ahbahgat01@gmail.com>
2022-03-28 17:33:10 +0200

    added authentication for orders routes

--- src/routes/api/orders.routes.ts
+++ src/routes/api/orders.routes.ts
@@ -5,15 +5,15 @@ import authMiddleware from '../../middlewares/authentication.middleware'
 const routes = Router()
 
 routes.route('/')
-    .get(controllers.getAllOrders)
+    .get(authMiddleware, controllers.getAllOrders)
     .post(authMiddleware, controllers.createOrder)
 routes.route('/:id')
-    .get(controllers.getOrder)
+    .get(authMiddleware, controllers.getOrder)
     .patch(authMiddleware, controllers.updateOrder)
     .delete(authMiddleware, controllers.deleteOrder)
 routes.route('/:id/products')
     .post(authMiddleware, controllers.addProduct)
-    .get(controllers.getOrderProducts)
+    .get(authMiddleware, controllers.getOrderProducts)
 routes.route('/:id/products/:pid')
     .delete(authMiddleware, controllers.deleteProduct)
 export default routes
